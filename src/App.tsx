8dea661ca18450852be01a726c40ec876379589b
aderibigbeganiu <aderibigbeganiu@gmail.com>
2024-06-08 03:22:07 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -12,52 +12,54 @@ function App() {
         <>
             <div>
                 {WebApp.initDataUnsafe?.user ? (
-                    <div>
-                        <h1>Welcome {WebApp.initDataUnsafe?.user?.username}</h1>
-                        User data:
-                        <pre>
-                            {JSON.stringify(
-                                WebApp.initDataUnsafe?.user,
-                                null,
-                                2
-                            )}
-                        </pre>
-                        Eniter Web App data:
-                        <pre>{JSON.stringify(WebApp, null, 2)}</pre>
-                    </div>
+                    <>
+                        <div>
+                            <h1>
+                                Welcome {WebApp.initDataUnsafe?.user?.username}
+                            </h1>
+                            Entire Web App data:
+                            <pre>{JSON.stringify(WebApp, null, 2)}</pre>
+                        </div>
+                        <div>
+                            <a href="https://vitejs.dev" target="_blank">
+                                <img
+                                    src={viteLogo}
+                                    className="logo"
+                                    alt="Vite logo"
+                                />
+                            </a>
+                            <a href="https://react.dev" target="_blank">
+                                <img
+                                    src={reactLogo}
+                                    className="logo react"
+                                    alt="React logo"
+                                />
+                            </a>
+                        </div>
+                        <h1>Vite + React</h1>
+                        <div className="card">
+                            <button
+                                onClick={() => setCount((count) => count + 1)}
+                            >
+                                count is {count}
+                            </button>
+                        </div>
+                        {/* Here we add our button with alert callback */}
+                        <div className="card">
+                            <button
+                                onClick={() =>
+                                    WebApp.showAlert(
+                                        `Hello World! Current count is ${count}`
+                                    )
+                                }
+                            >
+                                Show Alert
+                            </button>
+                        </div>
+                    </>
                 ) : (
                     <div>Make sure web app is opened from telegram client</div>
                 )}
-            </div>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-            </div>
-            {/* Here we add our button with alert callback */}
-            <div className="card">
-                <button
-                    onClick={() =>
-                        WebApp.showAlert(
-                            `Hello World! Current count is ${count}`
-                        )
-                    }
-                >
-                    Show Alert
-                </button>
             </div>
         </>
     );
